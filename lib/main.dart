a03c2d88acc16423b99b727691434e4b8def156d
k-saito-en <saito.kotaro.engineer@gmail.com>
2022-12-13 21:32:06 +0900

    # やったこと - ソースコード理解のために各種コメントを追加 - if文でクイズ問題の表示を切り替えるように実装

--- lib/main.dart
+++ lib/main.dart
@@ -7,43 +7,50 @@ import './answer.dart';
 //   runApp(MyApp());
 // }
 
+//StatefulWidgetであるMyAppウィジェットを受け取って画面にUI表示するmainメソッド
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
+
+  //stateクラスを受け取ってMyAppの「状態（state）」を生成する
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
   }
 }
 
+//stateを継承しているのでこのクラスはMyAppの「状態」を保持しているクラス
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'White'],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    },
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+
+    if (_questionIndex < questions.length) {}
+
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    const questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'White'],
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'questionText': 'Who\'s your favorite instructor?',
-        'answers': ['Max', 'Max', 'Max', 'Max'],
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
